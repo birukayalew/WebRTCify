dee698c4d4e6eef7ee2658532704990b4a69560c
biruk ayalew <se.biruk.ayalew@gmail.com>
2022-09-16 17:39:20 +0300

    Live streaming with Web Socket

--- webrtc/urls.py
+++ webrtc/urls.py
@@ -1,4 +1,5 @@
 """webrtc URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
@@ -19,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    path('chat/', include('chat.urls')),
+    path('', include('home.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
